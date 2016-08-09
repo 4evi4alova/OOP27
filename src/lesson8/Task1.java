170fdfd4c82e8b8a9ff0462368bb61b2bc8fa04d
Admin <4evi4alova@gmail.com>
2016-08-09 20:04:04 +0300

    Added old classes

---
+++ src/lesson8/Task1.java
@@ -0,0 +1,12 @@
+package lesson8;
+
+/**
+ * Created by Admin on 09.08.2016.
+ */
+public class Task1 {
+    public static void main (String [] args){
+System.out.print("Hello world ");
+    }
+
+}
+
