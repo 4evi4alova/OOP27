e0055d49d179061b3124a08860c3115d2f8885a6
Admin <4evi4alova@gmail.com>
2016-09-16 20:35:41 +0300

    initial commit

---
+++ src/test/Main.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Admin on 16.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
